a986900afc583b5f42feea8c9f631a203c6be098
ayaint2019 <ayaint2019@gmail.com>
2025-06-18 09:47:39 +0000

    yy

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/about.dart';
-import 'package:myapp/home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {"/": (context) => Home(), "/about": (context) => About()},
-    ),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
+class Home extends StatefulWidget {
+  const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            border: Border.all(width: 2, color: Colors.blue),
+            boxShadow: [
+              BoxShadow(color: Colors.blue, offset: Offset(12.0, 0.0), blurRadius: 2),
+            ]
+
+          ),
+          child: Text("Hello Flutter"),
+          
+          
+                ),
+        )),
+    );
+  }
+}
